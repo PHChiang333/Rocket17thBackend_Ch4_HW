3c0e956c137110730ca0bcef19089f7e413b4aed
PHChiang333 <frank79530@gmail.com>
2024-07-30 22:23:32 +0800

    feat:CH5 HW first commit

---
+++ Rocket17thBackend_Ch4_HW/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket17thBackend_Ch4_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+        }
+    }
+}
